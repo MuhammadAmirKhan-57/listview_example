0059306fe8c21b9103d6fa7e90c0281680f54157
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-08 23:02:28 +0500

    ListView/Verticle

--- lib/listview_screen.dart
+++ lib/listview_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ListViewScreen extends StatelessWidget {
   const ListViewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('ListView'),
+        backgroundColor: Colors.indigo.shade200,
+      ),
+      body: ListView.builder(
+        itemCount: 20,
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, i){
+          return ListTile(
+            leading: Icon(Icons.account_circle_outlined),
+            title: Text('Person'),
+            trailing: Icon(Icons.location_on_outlined, color: Colors.red,),
+          );
+        }
+        ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:listview_example/listview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: ListViewScreen(),
     );
   }
 }
