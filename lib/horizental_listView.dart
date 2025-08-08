d79a7872d153e5529c7d55f4565d6b582b26e263
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-08 23:15:26 +0500

    Listview Done

--- lib/horizental_listView.dart
+++ lib/horizental_listView.dart
@@ -11,26 +11,40 @@ class HorizentalListView extends StatelessWidget {
         title: const Text('ListView'),
         backgroundColor: Colors.indigo.shade200,
       ),
-      body: SizedBox(
-        height: 100, 
-        child: ListView.builder(
-          itemCount: 20,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, i) {
-            return Container(
-              width: 80,
-              height: 80,
-              margin: const EdgeInsets.all(8),
-              color: Colors.primaries[i % Colors.primaries.length],
-              child: const Center(
-                child: Text(
-                  'Item',
-                  style: TextStyle(color: Colors.white),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              itemCount: 20,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, i) {
+                return Container(
+                  width: 80,
+                  height: 80,
+                  margin: const EdgeInsets.all(8),
+                  color: Colors.primaries[i % Colors.primaries.length],
+                  child: const Center(
+                    child: Text('Item', style: TextStyle(color: Colors.white)),
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              'Here the listview builder is horizental direction',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
                 ),
-              ),
-            );
-          },
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
